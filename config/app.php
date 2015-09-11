e08a51aaf66a768fb9af8e9c040196dd6b68c72b
Jacob Martin <mindnumb@gmail.com>
2015-09-11 13:39:18 -0700

    Install debugbar

--- config/app.php
+++ config/app.php
@@ -144,6 +144,11 @@ return [
 		App\Providers\EventServiceProvider::class,
 		App\Providers\RouteServiceProvider::class,
 
+		/*
+		 * Our service providers...
+		 */
+
+		Barryvdh\Debugbar\ServiceProvider::class,
 	],
 
 	/*
@@ -191,6 +196,8 @@ return [
 		'URL'       => Illuminate\Support\Facades\URL::class,
 		'Validator' => Illuminate\Support\Facades\Validator::class,
 		'View'      => Illuminate\Support\Facades\View::class,
+
+		'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
 	],
 
