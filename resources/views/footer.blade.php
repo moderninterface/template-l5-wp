d172ba24202c6541bad6a909cff599596ed9d4fd
Jacob Martin <mindnumb@gmail.com>
2016-02-13 19:03:40 -0800

    .

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,13 +1,15 @@
 <footer id="footer">
 	<form action="{{ url('/newsletter') }}" method="post" id="newsletter-form">
 		<input type="hidden" name="_token" value="{{ csrf_token() }}" />
+
+		{{-- Honey pot to trick the bots --}}
 		<input type="text" id="thepot" name="thename" placeholder="Name" />
 
 		{{-- Errors / Success --}}
 		<div class="row">
 			<div class="large-12 columns errors hide"></div>
 			<div class="large-12 columns thanks hide">
-				<p class="alert-box success">You are now subscribed to the Sugar Street Newsletter!</p>
+				<p class="alert-box success">You are now subscribed to the newsletter!</p>
 			</div>
 		</div>
 
